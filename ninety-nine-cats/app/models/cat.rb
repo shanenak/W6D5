346a5b970676058795e4ed3fbae7689eb8ee88dd
Shannon Millar <shanenak@gmail.com>
2023-08-25 14:25:28 -0700

    Finish cat model and add cat controller

--- ninety-nine-cats/app/models/cat.rb
+++ ninety-nine-cats/app/models/cat.rb
@@ -1,19 +1,24 @@
+require 'action_view'
+require 'date'
+
 class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper
+
     CAT_COLORS = ['red', 'green', 'blue']
 
     validates :birth_date, :color, :name, :sex, presence: true
     validates :color, inclusion: { in: CAT_COLORS}
     validates :sex, inclusion: { in: %w(M F)}
-    valdiate :birth_date_cannot_be_future
+    validate :birth_date_cannot_be_future
 
     def birth_date_cannot_be_future
         if birth_date.present? && birth_date > Date.today
-            errors.add(:birth_date, "can't be in the future")
+            errors.add(birth_date, "can't be in the future")
         end
     end
 
     def age
-        Date.now.year - Date.parse(:birth_date).year
+        Date.today.year - birth_date.year
     end
 
 end
--- ninety-nine-cats/config/routes.rb
+++ ninety-nine-cats/config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :cats, except: :destroy
+  root "cats#index"
+
 end
