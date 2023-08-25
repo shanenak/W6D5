11e55e77ed83221ae1df8683f0883063426c7583
Raymond <raymondtan676@gmail.com>
2023-08-25 14:10:23 -0700

    Finish age method

--- ninety-nine-cats/app/models/cat.rb
+++ ninety-nine-cats/app/models/cat.rb
@@ -4,11 +4,16 @@ class Cat < ApplicationRecord
     validates :birth_date, :color, :name, :sex, presence: true
     validates :color, inclusion: { in: CAT_COLORS}
     validates :sex, inclusion: { in: %w(M F)}
+    valdiate :birth_date_cannot_be_future
 
     def birth_date_cannot_be_future
-       if birth_date.present? && birth_date > Date.today
-        errors.add(:birth_date, "can't be in the future")
-       end
+        if birth_date.present? && birth_date > Date.today
+            errors.add(:birth_date, "can't be in the future")
+        end
+    end
+
+    def age
+        Date.now.year - Date.parse(:birth_date).year
     end
 
 end
